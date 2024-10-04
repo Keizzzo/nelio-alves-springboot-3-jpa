2ace212fdf36bf6da38e2be41bdb5b12f1ffa076
Keizo Kobayashi <keizo.kobayashi@outlook.com.br>
2024-10-03 22:24:17 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -54,18 +54,10 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
-
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-
-        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
-        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
-        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
-        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
-
-        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
         //MANY TO MANY
         p1.getCategories().add(cat2);
@@ -77,6 +69,13 @@ public class TestConfig implements CommandLineRunner {
 
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        //CLASSE ASSOCIATIVA
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
     }
 }
