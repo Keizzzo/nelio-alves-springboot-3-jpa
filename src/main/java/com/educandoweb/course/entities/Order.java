b24081d9d8cd3a368668325a1e621d1c2029462f
Keizo Kobayashi <keizo.kobayashi@outlook.com.br>
2024-10-07 22:30:48 -0300

    Subtotal and Total

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -90,6 +90,15 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public Double getTotal(){
+        double sum = 0.0;
+
+        for(OrderItem o: items){
+            sum += o.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
